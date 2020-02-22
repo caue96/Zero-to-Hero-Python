99a75c3e45d55f2465581d869211e0dd03063a45
caue96 <cauemateus@hotmail.com>
2020-02-22 17:45:37 -0300

    Ready to continue the improvement in code

--- jogos (copy)/forca.py
+++
@@ -1,7 +0,0 @@
-def jogar():
-    print("*********************************")
-    print("***Bem vindo no jogo da Forca!***")
-    print("*********************************")
-
-if (__name__ == "__main__"):
-    jogar()
